19467a40fa425691742eab630720b5bddb3afb71
nadeem.ahmad1 <n.ahmad@epilot.cloud>
2024-08-24 12:13:47 +0200

    enhance navbar experience

--- src/portfolio/about/About.tsx
+++ src/portfolio/about/About.tsx
@@ -5,26 +5,26 @@ import './about.css'
 
 import WorkExperience from './WorkExperience'
 
+const text1 = `
+With over **seven years of experience** in **software engineering**, I am dedicated to building **impactful digital solutions** 
+that **prioritize user satisfaction** and high-quality performance. My expertise spans across **full-stack development**, 
+where I have successfully contributed to projects in diverse sectors including **energy solutions**,  **government services**, 
+and  **digital media**.`
+
+const text2 = `Currently, I work as a  **Senior Full Stack Engineer ** at  **Epilot GmbH** in Cologne, Germany, where I 
+collaborate closely with **cross-functional teams ** to develop  **end-to-end features ** that enhance product innovation. 
+My role involves everything from  **brainstorming ** and  **designing proof-of-concepts ** to developing  **minimum viable products**, 
+ensuring that each deliverable aligns with both technical feasibility and **user needs**.`
+
+const text3 = `
+As an **AI enthusiast**, I continuously explore new technologies and **integrate advanced tools** into my work. 
+My passion for learning is reflected in the **side projects** I undertake, where I delve into AI, machine learning, 
+and other cutting-edge fields. I also share my knowledge through **technical blogs**, making complex concepts accessible 
+to a broader audience. My technical arsenal includes a vast array of tools and technologies such as **JavaScript**, 
+**TypeScript**, **React**, **AWS Serverless**, **Docker**, and many more. Whether it's developing **robust APIs**, **optimizing databases**, 
+or **enhancing user interfaces**, I am committed to **delivering solutions** that are both **efficient and user-centric**.`
+
 function About() {
-  const text1 = `
-   With over **seven years of experience** in **software engineering**, I am dedicated to building **impactful digital solutions** 
-   that **prioritize user satisfaction** and high-quality performance. My expertise spans across **full-stack development**, 
-   where I have successfully contributed to projects in diverse sectors including **energy solutions**,  **government services**, 
-   and  **digital media**.`
-
-  const text2 = `Currently, I work as a  **Senior Full Stack Engineer ** at  **Epilot GmbH** in Cologne, Germany, where I 
-   collaborate closely with **cross-functional teams ** to develop  **end-to-end features ** that enhance product innovation. 
-   My role involves everything from  **brainstorming ** and  **designing proof-of-concepts ** to developing  **minimum viable products**, 
-   ensuring that each deliverable aligns with both technical feasibility and **user needs**.`
-
-  const text3 = `
-  As an **AI enthusiast**, I continuously explore new technologies and **integrate advanced tools** into my work. 
-   My passion for learning is reflected in the **side projects** I undertake, where I delve into AI, machine learning, 
-   and other cutting-edge fields. I also share my knowledge through **technical blogs**, making complex concepts accessible 
-   to a broader audience. My technical arsenal includes a vast array of tools and technologies such as **JavaScript**, 
-   **TypeScript**, **React**, **AWS Serverless**, **Docker**, and many more. Whether it's developing **robust APIs**, **optimizing databases**, 
-   or **enhancing user interfaces**, I am committed to **delivering solutions** that are both **efficient and user-centric**.`
-
   return (
     <div className="about">
       <div className="profile-summary">
--- src/portfolio/navigation/Navigation.tsx
+++ src/portfolio/navigation/Navigation.tsx
@@ -1,8 +1,13 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './navigation.css'
 import { NavLink } from 'react-router-dom'
 
+import Moon from '../../images/theme/moon.png'
+import Sun from '../../images/theme/sun.png'
+
 function Navigation() {
+  const [theme, setTheme] = useState('light')
+
   useEffect(() => {
     // on scroll apply styles to the navigation
     const handleScroll = () => {
@@ -23,30 +28,45 @@ function Navigation() {
 
   return (
     <div className="navigation">
-      <NavLink
-        className={({ isActive }) => {
-          return isActive ? 'navigation-item hovered-bg' : 'navigation-item'
-        }}
-        to={'/'}
-      >
-        Home
-      </NavLink>
-      <NavLink
-        className={({ isActive }) => {
-          return isActive ? 'navigation-item hovered-bg' : 'navigation-item'
-        }}
-        to={'/about'}
-      >
-        About
-      </NavLink>
-      <NavLink
-        className={({ isActive }) => {
-          return isActive ? 'navigation-item hovered-bg' : 'navigation-item'
-        }}
-        to={'/blogs'}
-      >
-        Blogs
-      </NavLink>
+      <div className="navigation-content">
+        <div className="left-content">
+          <NavLink
+            className={({ isActive }) => {
+              return isActive ? 'navigation-item hovered-bg' : 'navigation-item'
+            }}
+            to={'/'}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => {
+              return isActive ? 'navigation-item hovered-bg' : 'navigation-item'
+            }}
+            to={'/about'}
+          >
+            About
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => {
+              return isActive ? 'navigation-item hovered-bg' : 'navigation-item'
+            }}
+            to={'/blogs'}
+          >
+            Blogs
+          </NavLink>
+        </div>
+        <div className="right-content">
+          <div
+            className="theme-switcher"
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+            onKeyDown={() => ''}
+            role="button"
+            tabIndex={0}
+          >
+            <img alt="Nadeem Ahmad" src={theme === 'light' ? Sun : Moon} />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
